af4c4c7ee46457acec77c36f934f1246d62aa5bc
kalyanisalalkar <kalyanisallalkar@gmail.com>
2024-03-01 15:47:14 +0530

    How to write comments in shell-script

---
+++ Day2/comments.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#How to write comment in Shell-Script
+#Single line comment
+echo "this is single line comment"
+
---
+++ Day2/multipleLinecomment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+: '
+This is MultiLine Comments'
+
+echo "This is MultiLine Comment"
+
+: '
+Print some word'
+
